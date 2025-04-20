f1ad79f3ab8f5b4806227c1eac7cedd3f12e3c79
christy <pepupx2@naver.com>
2025-04-20 13:30:38 +0900

    chore: 레시피 서비스 초기 설정

---
+++ src/main/java/com/rego/recipingrecipesservice/RecipingRecipesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rego.recipingrecipesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipingRecipesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipingRecipesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rego/recipingrecipesservice/RecipingRecipesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rego.recipingrecipesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipingRecipesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
